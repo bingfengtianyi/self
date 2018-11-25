d987ac3e496199eab09bb3445f88ab1197c1dedb
jiangbo <jb112224@sina.com>
2018-11-25 23:22:12 +0800

    导出导入用户表完整数据

--- pyg_parent/pyg_web_user/src/main/java/com/itheima/pyg/controller/excel/CreateXSL.java
+++ pyg_parent/pyg_web_user/src/main/java/com/itheima/pyg/controller/excel/CreateXSL.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
@@ -24,8 +25,16 @@ public class CreateXSL {
     @RequestMapping("exportexcel")
     public void exportexcel(HttpServletResponse response) {
         List<User> userList = userService.getUserList();
-        String[] columnNames = {"用户名","密码","手机号","注册邮箱","创建时间","更新时间","会员来源","昵称","真实姓名","使用状态"};
-        String[] columns = {"username","password","phone","created"};
+        String[] columnNames = {
+                "用户名","密码","手机号","注册邮箱","创建时间","更新时间","会员来源","昵称","真实姓名","使用状态",
+                "头像地址","QQ号码","账户余额","手机是否验证","邮箱是否检测","性别","会员等级","积分","经验值",
+                "生日","最后登录时间"
+        };
+        String[] columns = {
+                "username","password","phone","email","created","updated","source_type","nick_name","status",
+                "head_pic","qq","account_balance","is_mobile_check","is_email_check","sex","user_level","points",
+                "experience_value","birthday","last_login_time"
+        };
         ExcelUtils.exportExcel(response,userList,columnNames,columns,"用户表","E:\\test.html");
     }
 
@@ -41,8 +50,24 @@ public class CreateXSL {
                 user.setUsername(data[i][0]+"02");
                 user.setPassword(data[i][1]);
                 user.setPhone(data[i][2]);
-                user.setCreated(format.parse(data[i][3]));
-                user.setUpdated(format.parse(data[i][4]));
+                user.setEmail(data[i][3]);
+                user.setCreated(format.parse(data[i][4]));
+                user.setUpdated(format.parse(data[i][5]));
+                user.setSourceType(data[i][6]);
+                user.setNickName(data[i][7]);
+                user.setName(data[i][8]);
+                user.setStatus(data[i][9]);
+                user.setHeadPic(data[i][10]);
+                user.setQq(data[i][11]);
+                user.setAccountBalance(Long.valueOf(data[i][12]));
+                user.setIsMobileCheck(data[i][13]);
+                user.setIsEmailCheck(data[i][14]);
+                user.setSex(data[i][15]);
+                user.setUserLevel(Integer.valueOf(data[i][16]));
+                user.setPoints(Integer.valueOf(data[i][17]));
+                user.setExperienceValue(Integer.valueOf(data[i][18]));
+                user.setBirthday(format.parse(data[i][19]));
+                user.setLastLoginTime(format.parse(data[i][20]));
                 userService.save(user);//这是一个添加方法，dao层写入sql语句即可
             }
             map.put("success", true);
